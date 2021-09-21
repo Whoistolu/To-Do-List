47aec038fc31ad01eeff3b767fb3330212bb1121
Whoistolu <digittolu25@gmail.com>
2021-09-21 14:05:50 +0100

    Setup project with webpack

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,7 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
