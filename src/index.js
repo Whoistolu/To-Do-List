47ae64543ae0d4e93454a2612e1a4db71660eb78
Whoistolu <digittolu25@gmail.com>
2021-09-22 13:21:05 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './style.css';
+
+const items = [
+  {
+    description: 'wash the dishes',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'complete To Do List project',
+    completed: false,
+    index: 1,
+  },
+];
+
+const todos = document.querySelector('.todos');
+items.forEach((item) => {
+  const html = `<input type="checkbox">
+  <input value=${item.description} type="text"></input>
+  <i class="fas fa-ellipsis-v"></i>`;
+  const div = document.createElement('div');
+  div.innerHTML = html;
+  const input = div.querySelector('input[type=checkbox]');
+  input.checked = item.completed;
+  div.classList.add('todo-item');
+  div.id = item.index;
+
+  todos.appendChild(div);
+});
