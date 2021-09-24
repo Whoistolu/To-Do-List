9af518d8e2f00ea465014c7cc3a78f319eef18f4
Whoistolu <digittolu25@gmail.com>
2021-09-24 22:22:14 +0100

    Implement the remove function

--- src/active.js
+++ src/active.js
@@ -1,8 +1,15 @@
 export const saveLocalStorage = (data) => {
+  const todoItems = document.querySelectorAll('.todo-item');
+  todoItems.forEach((item, idx) => {
+    console.log(item)
+    item.id = idx;
+  });
+  data.forEach((d, idx) => {
+    d.index = idx;
+  });
   localStorage.setItem('toDoStorage', JSON.stringify(data));
 };
 
 export const updateCompleted = (item, input) => {
   item.completed = input.checked;
 };
-
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import './style.css';
 import { saveLocalStorage, updateCompleted } from './active.js';
 
-let items = [];
+let itemsArray = [];
 
 const storageData = JSON.parse(localStorage.getItem('toDoStorage'));
 if (storageData) {
-  items = storageData;
+  itemsArray = storageData;
 }
 
 const todos = document.querySelector('.todos');
-items.forEach((item) => {
+console.log(itemsArray);
+itemsArray.forEach((item) => {
   const html = `<input type="checkbox">
-  <input value=${item.description} type="text"></input>
-  <i class="fas fa-ellipsis-v"></i>`;
+  <input class="edit" value=${item.description} type="text"></input>
+  <i class="fas fa-ellipsis-v vertical"></i>
+  <i class="fas fa-trash-alt trash"></i>`;
   const div = document.createElement('div');
   div.innerHTML = html;
   const input = div.querySelector('input[type=checkbox]');
@@ -22,21 +24,61 @@ items.forEach((item) => {
 
   input.addEventListener('change', () => {
     updateCompleted(item, input);
-    saveLocalStorage(items);
+    saveLocalStorage(itemsArray);
   });
 
   todos.appendChild(div);
+  const vertical = div.querySelector('.vertical');
+  const trash = div.querySelector('.trash');
+  const edit = div.querySelector('.edit');
+  edit.addEventListener('change', () => {
+    console.log('new text: ', edit.value, ' my div.id', div.id);
+    itemsArray.forEach((item) => {
+      if (item.index.toString() === div.id) {
+        item.description = edit.value;
+      }
+    });
+    saveLocalStorage(itemsArray);
+  });
+  vertical.addEventListener('click', () => {
+    vertical.style.display = 'none';
+    trash.style.display = 'block';
+  });
+  trash.addEventListener('click', () => {
+    div.remove();
+    itemsArray = itemsArray.filter((something) => {
+      if (something.index.toString() === div.id) {
+        return false;
+      }
+      return true;
+    });
+    saveLocalStorage(itemsArray);
+  });
 });
-
 
 const form = document.getElementById('form-id');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const result = form.querySelector('input').value;
-  console.log('length: ', items.length);
-  items.push({completed: false, description: result, index: items.length});
-  console.log(items);
-  saveLocalStorage(items);
+  itemsArray.push({ completed: false, description: result, index: itemsArray.length });
+  saveLocalStorage(itemsArray);
   window.location.reload();
+});
 
+const clearButton = document.querySelector('#button');
+clearButton.addEventListener('click', () => {
+  const todoArray = document.querySelectorAll('.todo-item');
+  todoArray.forEach((div) => {
+    const input = div.querySelector('input');
+    if (input.checked) {
+      div.remove();
+      itemsArray = itemsArray.filter((something) => {
+        if (something.index.toString() === div.id) {
+          return false;
+        }
+        return true;
+      });
+      saveLocalStorage(itemsArray);
+    }
+  });
 });
